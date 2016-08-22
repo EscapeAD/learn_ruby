21adb8c69edd3fab446445482c639257430950d1
Adam Tse <mr.adam.tse@gmail.com>
2016-08-21 20:53:52 -0400

    added instance for multiple words in sentence to be tranlsated

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -6,8 +6,6 @@ def translate(sentence)
   array          = sentence.split(" ")
   output         = []
   translation    = ""
-  # con            = alph.select { |con| con =~ /[aeiou]/ }
-  # con            = alph.select { |v| v != vowels }
   if array.count == 1
     one          = array[0].split(//)
     if vowels.include? one[0]
@@ -20,32 +18,25 @@ def translate(sentence)
       translation = one.drop(3).join + one.first(3).join + "ay"
     end
   else
-    # array.each do |word|
-    # final          = []
-    # check = word.split(//)
-    # output << check
-    # if alph.include? word.check[0] && alph.include? word[1] && alph.include? word[2]
-    #   output << check[0..2]
-    # elsif alph.include? word[0] && alph.include? word[1]
-    #   output << check[0..1]
-    # elsif alph.include? word[0]
-    #   output << check[0]
-    # end
+    array.each do |word|
+      one           = word.split(//)
+      if vowels.include? one[0]
+        finish      = one.join + "ay"
+        output << finish
+      elsif vowels.include? one[1]
+        finish = one.drop(1).join + one.first + "ay"
+        output << finish
+      elsif vowels.include? one[2]
+        finish = one.drop(2).join + one.first(2).join + "ay"
+        output << finish
+      elsif vowels.include? one[3]
+        finish = one.drop(3).join + one.first(3).join + "ay"
+        output << finish
+      end
+    end
+      translation = output.join(" ")
 end
 
 
-  #
-  # translation = output.join
-  # return translation
   return translation
 end
-
-
-# front = original_array.count {|vowel| vowels.include? vowel}
-# collector = phrase2.first(front-1)
-# if front > 0
-#   phrase2 << collector
-#   (front..1).each do |number|
-#     phrase2.delete_at(number)
-#   end
-# end
